c7745f6de77abeaa413f4900f7d044dbce4d144e
Mark Quinn <mobious_99@yahoo.com>
2025-06-29 15:18:46 -0400

    fix: remediate test vulnerabilities

--- tests/utils/test_output_handler_basic.py
+++ tests/utils/test_output_handler_basic.py
@@ -1,15 +1,19 @@
-import pytest
+import unittest
 
 from app.output_handler import OutputDispatcher
 from app.utils.types import OutputMode
 
 
-def test_output_dispatcher_init():
-    dispatcher = OutputDispatcher()
-    assert isinstance(dispatcher.output_modes, list)
+class TestOutputDispatcherBasic(unittest.TestCase):
+    def test_output_dispatcher_init(self):
+        dispatcher = OutputDispatcher()
+        self.assertIsInstance(dispatcher.output_modes, list)
+
+    def test_get_dispatch_method(self):
+        dispatcher = OutputDispatcher()
+        method = dispatcher._get_dispatch_method(OutputMode.LOG)
+        self.assertTrue(callable(method))
 
 
-def test_get_dispatch_method():
-    dispatcher = OutputDispatcher()
-    method = dispatcher._get_dispatch_method(OutputMode.LOG)
-    assert callable(method)
+if __name__ == "__main__":
+    unittest.main()
